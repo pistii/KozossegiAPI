b9e507ca4294927ec9bcf975e642540d9cd46e2e
pistii <pilisi.istvan@gmail.com>
2024-04-21 17:46:27 +0200

    Get removed, can be used generic instead

--- Repo/IPersonalRepository.cs
+++ Repo/IPersonalRepository.cs
@@ -4,7 +4,6 @@ namespace KozoskodoAPI.Repo
 {
     public interface IPersonalRepository<T> : IGenericRepository<T>
     {
-        Task<Personal> Get(int id);
         IQueryable<Personal> FilterPersons(int userId);
     }
 }
