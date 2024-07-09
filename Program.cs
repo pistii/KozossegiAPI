2ed5d4c043d7a607c2c51bda14957985b2882302
pistii <pilisi.istvan@gmail.com>
2024-07-09 18:17:55 +0200

    add dependency

--- Program.cs
+++ Program.cs
@@ -8,20 +8,18 @@ using KozoskodoAPI.Auth;
 using KozoskodoAPI.Realtime;
 using KozoskodoAPI.Auth.Helpers;
 using KozoskodoAPI.Controllers;
-using KozoskodoAPI.Realtime.Helpers;
 using KozoskodoAPI.Realtime.Connection;
 using KozoskodoAPI.Controllers.Cloud;
 using KozoskodoAPI.Repo;
 using KozoskodoAPI.SMTP;
-using KozoskodoAPI.SMTP.Storage;
 using KozoskodoAPI.Security;
 using KozoskodoAPI.Services;
 using KozoskodoAPI.Models;
 using KozoskodoAPI.DTOs;
 using KozossegiAPI.SMTP;
-using System.Configuration;
-using KozossegiAPI.Services;
 using KozossegiAPI.Controllers.Cloud;
+using KozossegiAPI.Storage;
+using KozossegiAPI.Controllers.Cloud.Helpers;
 
 namespace KozoskodoAPI
 {
@@ -35,7 +33,7 @@ namespace KozoskodoAPI
             services.AddDbContext<DBContext>(options =>
                 options.UseMySql(
                     builder.Configuration.GetConnectionString("MediaDB"),
-                    ServerVersion.Parse("10.4.6-mariadb")));
+                    ServerVersion.Parse("10.4.20-mariadb"))); //10.4.6-mariadb
 
             services.AddAuthentication(options =>
                 {
@@ -78,7 +76,8 @@ namespace KozoskodoAPI
             services.AddScoped<IMailSender, SendMail>();
             services.AddSingleton<IVerificationCodeCache, VerificationCodeCache>();
             services.AddScoped<IEncodeDecode, EncodeDecode>();
-
+            services.AddScoped<IFileHandlerService, FileHandlerService>();
+            services.AddSingleton<IChatStorage, ChatStorage>();
             services.AddHostedService<NotificationService>();
 
             services.AddMemoryCache();
