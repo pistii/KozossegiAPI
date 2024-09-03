72696fc66fa1f000fa0c6aab16a126f80e662213
pistii <pilisi.istvan@gmail.com>
2024-09-03 16:23:24 +0200

    Authentication and password reset middleware

---
+++ Auth/AuthenticateMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using KozoskodoAPI.Auth.Helpers;
+using KozoskodoAPI.Auth;
+using KozossegiAPI.Storage;
+using KozoskodoAPI.Models;
+
+namespace KozossegiAPI.Auth
+{
+    public class AuthenticateMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public AuthenticateMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context, IJwtUtils jwtUtils, IVerificationCodeCache cache)
+        {
+            var tokenfromPw = context.Request.Headers["Authentication"].FirstOrDefault()?.Split(" ").Last();
+
+            var email_otp = jwtUtils.ValidateAccessToken(tokenfromPw);
+
+            if (email_otp != null)
+            {
+                var session = email_otp.Split(';');
+                var email = session[0];
+                var otp = session[1];
+                var emailExist = cache.GetValue(otp);
+                if (emailExist == null || email != emailExist)
+                {
+                    context.Response.ContentType = "text/plain";
+                    context.Response.StatusCode = 401; //UnAuthorized
+                    await context.Response.WriteAsync("Unauthorized");
+                }
+            }
+            await _next(context);
+        }
+    }
+}
---
+++ Auth/Helpers/AuthenticateAttribute.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using KozoskodoAPI.Auth.Helpers;
+using KozoskodoAPI.Models;
+
+namespace KozossegiAPI.Auth.Helpers
+{
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AuthenticateAttribute : Attribute
+    {
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            // skip authorization if action is decorated with [AllowAnonymous] attribute
+            var allowAnonymous = context.ActionDescriptor.EndpointMetadata.OfType<AllowAnonymousAttribute>().Any();
+            if (allowAnonymous)
+                return;
+
+            // Authorization
+            var email = context.HttpContext.Items["Email"];
+
+            if (email == null)
+            {
+                // not logged in or role not authorized
+                context.Result = new JsonResult(new { message = "Unathenticated" }) { StatusCode = StatusCodes.Status401Unauthorized };
+            }
+        }
+    }
+
+
+    [AttributeUsage (AttributeTargets.Class | AttributeTargets.Method)]
+    public class UserIdCheckAttribute : Attribute, IAuthorizationFilter
+    {
+        private readonly string _headerKey;
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var userIdFromRoute = context.HttpContext.Request.Body.ToString();
+            var userIdFromHeader = context.HttpContext.Request.Headers[_headerKey].FirstOrDefault();
+
+            if (userIdFromRoute == null || userIdFromHeader == null || userIdFromRoute != userIdFromHeader)
+            {
+                context.Result = new UnauthorizedResult();
+            }
+        }
+    }
+}
