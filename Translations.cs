9b8ba93a01e054b804ff70b6bc36515df384ba0a
pistii <pilisi.istvan@gmail.com>
2024-09-16 18:06:40 +0200

    add translations

---
+++ Translations.cs
@@ -0,0 +1,10 @@
+﻿namespace KozossegiAPI
+{
+    public class Translations
+    {
+        public string confirm_registration { get; set; }
+        public string click_link { get; set; }
+        public string one_step { get; set; }
+        public string email_because { get; set; }
+    }
+}
