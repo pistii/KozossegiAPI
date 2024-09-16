69485b82f8065315508d2920f4b8825909bc58dc
pistii <pilisi.istvan@gmail.com>
2024-09-16 18:07:50 +0200

    Add regular expression attribute for password changes

--- DTOs/SettingsDto.cs
+++ DTOs/SettingsDto.cs
@@ -54,9 +54,11 @@ namespace KozossegiAPI.DTOs
             this.pass2 = pass2;
         }
         [StringLength(40, MinimumLength = 8)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$", ErrorMessage = "Invalid password format. Must contain at least one uppercase, one lowercase, one number.")]
         public string? pass1 { get; set; } = string.Empty;
         
         [StringLength(40, MinimumLength = 8)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$", ErrorMessage = "Invalid password format. Must contain at least one uppercase, one lowercase, one number.")]
         public string? pass2 { get; set; } = string.Empty;
     }
 }
