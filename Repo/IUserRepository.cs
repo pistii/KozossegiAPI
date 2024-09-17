cf5a5fc627daabbb8dfc6e1e67a0bd5d598e6810
pistii <pilisi.istvan@gmail.com>
2024-09-17 18:10:55 +0200

    add email activator interfaces

--- Repo/IUserRepository.cs
+++ Repo/IUserRepository.cs
@@ -10,5 +10,7 @@ namespace KozoskodoAPI.Repo
         Task<Personal?> GetPersonalWithSettingsAndUserAsync(int userId);
         Task<user?> GetUserByEmailOrPassword(string email = null, string password = null);
         Task<user?> GetUserByEmailAsync(string email, bool withPersonal = true);
+        Task SendActivationEmail(string email, user user);
+        Task<bool> CanUserRequestMoreActivatorToday(string to);
     }
 }
