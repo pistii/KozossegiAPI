a4fcad16e78a6bb5f04fb9720fd074804c6f75ca
pistii <pilisi.istvan@gmail.com>
2024-09-17 18:27:34 +0200

    add elementary encrypt/decrypt methods using generated keys

--- Security/EncodeDecode.cs
+++ Security/EncodeDecode.cs
@@ -2,6 +2,7 @@
 using KozoskodoAPI.Models;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Newtonsoft.Json.Linq;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -56,6 +57,85 @@ namespace KozoskodoAPI.Security
             }
         }
 
+        public string EncryptAesManaged(string raw)
+        {
+            try
+            {
+                // Create Aes that generates a new key and initialization vector (IV).
+                // Same key must be used in encryption and decryption
+                using (AesManaged aes = new AesManaged())
+                {
+                    // Encrypt string
+                    byte[] encrypted = Encrypter(raw, aes.Key, aes.IV);
+                    // Print encrypted string
+                    Console.WriteLine($"Encrypted data: {System.Text.Encoding.UTF8.GetString(encrypted)}");
+                    //decrypt the bytes to a string.
+                    string decrypted = Decrypter(encrypted, aes.Key, aes.IV);
+                    // Print decrypted string. It should be same as raw data
+                    Console.WriteLine($"Decrypted data: {decrypted}");
+                    return decrypted;
+                }
+                return null;
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.Message);
+            }
+            Console.ReadKey();
+            return null;
+
+        }
+
+        static byte[] Encrypter(string plainText, byte[] Key, byte[] IV)
+        {
+            byte[] encrypted;
+            // Create a new AesManaged.
+            using (AesManaged aes = new AesManaged())
+            {
+                // Create encryptor
+                ICryptoTransform encryptor = aes.CreateEncryptor(Key, IV);
+                // Create MemoryStream
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    // Create crypto stream using the CryptoStream class. This class is the key to encryption
+                    // and encrypts and decrypts data from any given stream. In this case, we will pass a memory stream
+                    // to encrypt
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        // Create StreamWriter and write data to a stream
+                        using (StreamWriter sw = new StreamWriter(cs))
+                            sw.Write(plainText);
+                        encrypted = ms.ToArray();
+                    }
+                }
+            }
+            // Return encrypted data
+            return encrypted;
+        }
+
+        static string Decrypter(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            string plaintext = null;
+            // Create AesManaged
+            using (AesManaged aes = new AesManaged())
+            {
+                // Create a decryptor
+                ICryptoTransform decryptor = aes.CreateDecryptor(Key, IV);
+                // Create the streams used for decryption.
+                using (MemoryStream ms = new MemoryStream(cipherText))
+                {
+                    // Create crypto stream
+                    using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    {
+                        // Read crypto stream
+                        using (StreamReader reader = new StreamReader(cs))
+                            plaintext = reader.ReadToEnd();
+                    }
+                }
+            }
+            return plaintext;
+        }
+
         public string DecryptFromString(string base64CipherText)
         {
             var secret = "I love chocolate";
@@ -88,5 +168,7 @@ namespace KozoskodoAPI.Security
         string Decrypt(string cipherBytes, string pass);
         
         string DecryptFromString(string base64CipherText);
+
+        public string EncryptAesManaged(string raw);
     }
 }
