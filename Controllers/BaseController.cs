15f2140e097c56903dd18b6c0220c21342909e6d
pistii <pilisi.istvan@gmail.com>
2025-04-03 22:39:58 +0200

    Create baseController

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using KozossegiAPI.Models;
+
+namespace KozossegiAPI.Controllers
+{
+    public abstract class BaseController<T> : ControllerBase where T : ControllerBase
+    {
+        protected int GetUserId()
+        {
+            var user = HttpContext.Items["User"] as user;
+            if (user == null)
+            {
+                throw new UnauthorizedAccessException("User not found in request headers.");
+            }
+            return user.userID;
+        }
+
+        protected user GetUser()
+        {
+            var user = HttpContext.Items["User"] as user;
+            if (user == null)
+            {
+                throw new UnauthorizedAccessException("User not found in request headers.");
+            }
+            return user;
+        }
+    }
+}
