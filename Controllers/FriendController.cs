d982bc99efee2c7f7917d4aad11fea0bcce9a6d9
pistii <pilisi.istvan@gmail.com>
2025-04-08 15:13:48 +0200

    Rewrite whole friend controller. Unstead of using models for request parameter, rely on public ids

--- Controllers/FriendController.cs
+++ Controllers/FriendController.cs
@@ -1,4 +1,5 @@
-﻿using KozossegiAPI.Interfaces;
+﻿using KozossegiAPI.Auth.Helpers;
+using KozossegiAPI.Interfaces;
 using KozossegiAPI.Models;
 using KozossegiAPI.Realtime;
 using KozossegiAPI.Realtime.Connection;
@@ -10,45 +11,23 @@ namespace KozossegiAPI.Controllers
 {
     [ApiController]
     [Route("/api/[controller]")]
-    public class FriendController : ControllerBase
+    public class FriendController : BaseController<FriendController>
     {
         private readonly IFriendRepository _friendRepository;
-        private readonly IPersonalRepository _personalRepository;
         private readonly INotificationRepository _notificationRepository;
-        private readonly IHubContext<NotificationHub, INotificationClient> _notificationHub;
-        private readonly IMapConnections _connections;
+        private readonly IUserRepository<user> _userRepository;
 
+        private readonly IHubContext<NotificationHub, INotificationClient> _notificationHub;
         public FriendController(
             IFriendRepository friendRepository,
-            IPersonalRepository personalRepository,
             INotificationRepository notificationRepository,
-            IHubContext<NotificationHub, INotificationClient> hub,
-            IMapConnections connections)
+            IUserRepository<user> userRepository,
+            IHubContext<NotificationHub, INotificationClient> notificationHub)
         {
             _friendRepository = friendRepository;
-            _personalRepository = personalRepository;
             _notificationRepository = notificationRepository;
-            _notificationHub = hub;
-            _connections = connections;
-
-        }
-
-        /// <summary>
-        /// Tartalmazza az user táblát, ezt nem szabad vissza küldeni a kliensnek!
-        /// </summary>
-        /// <param name="userId"></param>
-        /// <returns></returns>
-        public async Task<List<Personal>> GetAllFriend(int userId)
-        {
-            var friends = _friendRepository.GetAllFriendAsync(userId).Result.ToList();
-            return friends;
-        }
-
-        [HttpGet("getFriends/{userId}/{currentPage}/{qty}")]
-        public async Task<List<Personal>> GetFriends(int userId)
-        {
-            var friends = await _friendRepository.GetAllFriendAsync(userId);
-            return friends.ToList();
+            _userRepository = userRepository;
+            _notificationHub = notificationHub;
         }
 
         /// <summary>
@@ -58,128 +37,120 @@ namespace KozossegiAPI.Controllers
         /// <param name="currentPage"></param>
         /// <param name="qty"></param>
         /// <returns></returns>
-        [HttpGet("{personalId}/{currentPage}/{qty}")]
-        public async Task<IActionResult> GetAll(int personalId, int currentPage = 1, int qty = 9)
-        {
-            var users = await _friendRepository.GetAll(personalId);
-            var some = users?.Skip((currentPage - 1) * qty).Take(qty);
 
-            return Ok(some);
+        [HttpGet("getAll/{publicId}/{currentPage}/{qty}")]
+        public async Task<IActionResult> GetAll(string publicId, int currentPage = 1, int qty = 9)
+        {
+            var user = await _friendRepository.GetByPublicIdAsync<user>(publicId);
+            if (user == null) return BadRequest("Invalid user Id");
+
+            var friends = await _friendRepository.GetAllFriendAsync(user.userID);
+            var sorted = friends?.Skip((currentPage - 1) * qty).Take(qty);
+
+            return Ok(sorted);
         }
 
-        [HttpPost("create/friendRequest")]
-        public async Task<IActionResult> CreateFriendRequest(CreateNotification notification)
+        [Authorize]
+        [HttpGet("request/{userId}")]
+        public async Task<IActionResult> CreateFriendRequest(string userId)
         {
-            var author = await _friendRepository.GetUserWithNotification(notification.AuthorId);
-            var receiver = await _friendRepository.GetUserWithNotification(notification.UserId);
+            var authorId = GetUserId();
+            var author = await _friendRepository.GetUserWithNotification(authorId);
+            var receiverUser = await _notificationRepository.GetByPublicIdAsync<user>(userId);
 
-            if (receiver == null || author == null) return NotFound("Invalid user id.");
-            else if (notification.NotificationType != NotificationType.FriendRequest) return BadRequest("Invalid request");
+            if (receiverUser == null || author == null) return NotFound("Invalid user id.");
+            if (authorId == receiverUser.userID) return BadRequest();
+            
+            Friend friend = new(receiverUser.userID, authorId, 3);
+            var existing = await _friendRepository.FriendshipExists(friend);
 
-            //Ellenőrizzük, hogy a küldő user nem kapott-e már barátkérelmet.
-            var authorReceivedRequestBefore = author!.UserNotification
-               .FirstOrDefault(n => n.UserId == notification.AuthorId &&
-               n.notification.NotificationType == NotificationType.FriendRequest);
-
-            Notification createdNotification = new();
-            //Létezik korábbi barátkérelem.
-            if (authorReceivedRequestBefore != null)
-                {
-                createdNotification = await _notificationRepository.CreateFriendRequestAcceptedNotification(notification);
-                await _friendRepository.MakeThemFriend(notification);
-                }
-                else
-                {
-                notification.Message = "";
-                createdNotification = await _notificationRepository.CreateNotification(notification);
-                }
-
-            // Értesítjük az usert hubon keresztül.
-            if (_connections.ContainsUser(notification.UserId))
-                {
-                //Az user összes létező kapcsolatának kikeresése.
-                var allUserConnection = _connections.keyValuePairs.Where(c => c.Value == notification.UserId).ToList();
-                //Kigyűjtjük a kapcsolati kulcsokat
-                List<string> keys = (from kvp in allUserConnection select kvp.Key).ToList();
-
-                //Az elküldendő objektum
-                var noti = new GetNotification(createdNotification.Id, author.id, receiver.id, createdNotification.CreatedAt,
-                    "", false, NotificationType.FriendRequest, "New friend request.", author.avatar);
-                    //Értesítés küldése az összes létező kapcsolat felé.
-                await _notificationHub.Clients.Clients(keys).ReceiveNotification(notification.UserId, noti);
-                    }
-            return Ok();
-                }
-
-        [HttpPut("add")] //if user accepts or rejects the request
-        public async Task<IActionResult> Put(Friend_notificationId friendship)
-        {
-            var friendshipExist = _friendRepository.FriendshipExists(friendship);
-
-            if (friendshipExist == null || friendship.FriendId != friendship.UserId) { //Utóbbi feltétel azt vizsgálja, hogy ne tudja ismerősnek jelölni saját magát...
-                try
-                {
-                    //Notification? notificationModified = _notificationRepository.GetNotification(friendship).Result;
-                    Notification? notificationModified = await _notificationRepository.GetByIdAsync<Notification>(friendship.NotificationId);
-                    if (notificationModified != null)
-                    {
-                        notificationModified.isNew = false;
-                    }
-
-                    if (friendship.StatusId == 1) //Baráti kérelem érkezett
-                    {
-                        await _friendRepository.Put(friendship);
-                        
-                        if (notificationModified != null)
-                        {
-                            notificationModified.notificationContent = "Mostantól ismerősök vagytok.";
-                            notificationModified.notificationType = NotificationType.FriendRequestAccepted;
-                            await _notificationRepository.UpdateAsync(notificationModified);
-                        }
-                        await _friendRepository.SaveAsync();
-                        return Ok(notificationModified);
-                    }
-                    else if (friendship.StatusId == 4) //Baráti kérelem elutasítva
-                    {
-                        if (notificationModified != null)
-                        {
-                            notificationModified.notificationContent = "Ismerős kérelem elutasítva.";
-                            notificationModified.notificationType = NotificationType.FriendRequestReject;
-                            await _notificationRepository.UpdateAsync(notificationModified);
-                        }
-
-                        await _friendRepository.Delete(friendship);
-                        await _friendRepository.SaveAsync();
-                        return Ok(notificationModified);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    return BadRequest();
-                }
-            }
-            return NoContent();
-        }
-
-        [HttpDelete]
-        public async Task<IActionResult> Delete(Friend request)
-        {
-            var friendship = await _friendRepository.FriendshipExists(request);
-            if (friendship != null)
+            //The other user requested before, mark them as friends.
+            if (existing != null && friend.UserId == receiverUser.userID)
             {
-                await _friendRepository.Delete(friendship);
-                await _friendRepository.SaveAsync();
-                return Ok("removed");
+                existing.StatusId = 1;
+                await _friendRepository.UpdateThenSaveAsync<Friend>(existing);
             }
+            else
+            {
+                await _friendRepository.InsertSaveAsync(friend); //Save friend request
+            }
+
+            //Save notification
+            Notification notification = new(authorId, "", NotificationType.FriendRequest);
+            await _notificationRepository.InsertSaveAsync(notification);
+
+            //Send notification
+            GetNotification getNotification = new(notification, author.avatar);
+            await _notificationHub.Clients.Users(receiverUser.userID.ToString()).ReceiveNotification(receiverUser.userID, getNotification);
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpGet("request/reject/{notificationId}")]
+        public async Task<IActionResult> RejectFriendRequest(string notificationId)
+        {
+            var userId = GetUserId();
+            Notification? result = await _notificationRepository.GetByPublicIdAsync<Notification>(notificationId);
+            if (result == null) return NotFound();
+
+            Friend friend = new(userId, result.AuthorId, 3);
+            Friend? initialFriendshipExist = await _friendRepository.FriendshipExists(friend);
+            if (initialFriendshipExist == null) return NotFound();
+
+            //Lehet hogy már barátok, vagy egyáltalán nem történt ismerősnek jelölés sem.
+            else if (initialFriendshipExist.StatusId != 3) return BadRequest();
+
+            await _notificationRepository.RemoveAsync(result);
+            _friendRepository.Delete(initialFriendshipExist);
             return NoContent();
         }
 
-        [HttpGet("relation")]
-        public async Task<string> GetFamiliarityStatus(int userId, int viewerId)
+        [Authorize]
+        [HttpPut("request/confirm/{notificationId}")]
+        public async Task<IActionResult> ConfirmFriendRequest(string notificationId)
         {
-            string friendship = await _friendRepository.GetUserRelation(userId, viewerId);
-            return friendship;
+            var userId = GetUserId();
+            Notification? result = await _notificationRepository.GetByPublicIdAsync<Notification>(notificationId);
+            if (result == null) return NotFound();
+
+            Friend friend = new(userId, result.AuthorId, 3);
+            Friend? initialFriendshipExist = await _friendRepository.FriendshipExists(friend);
+            if (initialFriendshipExist == null) return NotFound();
+
+            //Lehet hogy már barátok, vagy egyáltalán nem történt ismerősnek jelölés sem.
+            else if (initialFriendshipExist.StatusId != 3) return BadRequest();
+
+            initialFriendshipExist.StatusId = 1;
+            initialFriendshipExist.FriendshipSince = DateTime.Now;
+            await _friendRepository.UpdateThenSaveAsync(initialFriendshipExist);
+            return Ok();
         }
 
+
+        [Authorize]
+        [HttpDelete("delete/{friendId}")]
+        public async Task<IActionResult> Delete(string friendId)
+        {
+            var userId = GetUserId();
+
+            var userObj = await _userRepository.GetByPublicIdAsync<user>(friendId);
+            Friend friend = new()
+            {
+                FriendId = userObj.userID,
+                UserId = userId
+            };
+
+            var friendship = await _friendRepository.FriendshipExists(friend);
+            if (friendship == null) return NotFound();
+
+            bool shouldDelete = (int)UserRelationshipStatus.Friend == friendship.StatusId ||
+                    (int)UserRelationshipStatus.FriendRequestSent == friendship.StatusId;
+            if (!shouldDelete) return BadRequest("Friend request cannot be deleted.");
+
+            _friendRepository.Delete(friendship);
+            await _friendRepository.SaveAsync();
+            return Ok();
+            
+        }
     }
 }
--- Models/Friend.cs
+++ Models/Friend.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Newtonsoft.Json;
 
@@ -10,23 +11,42 @@ namespace KozossegiAPI.Models
     [Table("Friend")]
     public partial class Friend
     {
+        public Friend()
+        {
+            
+        }
+
+        /// <summary>
+        /// Create a friendship object to save.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="friendId"></param>
+        /// <param name="statusId"></param>
+        public Friend(int userId, int friendId, int statusId)
+        {
+            this.FriendId = friendId;
+            this.UserId = userId;
+            this.StatusId = statusId;
+        }
+
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Column(TypeName = "int(11)")]
         public int? FriendshipID { get; set; }
+
+        [Description("The user who requested the friendship.")]
         [Column(TypeName = "int(11)")]
         public int UserId { get; set; }
         [Column(TypeName = "int(11)")]
+        [Description("The user who receives the friendship request.")]
         public int FriendId { get; set; }
         [Column(TypeName = "int(11)")]
         public int? StatusId { get; set; } //1= friends, 2 = nonFriend, 3 = Sent, 4 = Rejected
-        
+
         public DateTime? FriendshipSince { get; set; }
         public FriendshipStatus? friendship_status { get; set; }
         [JsonIgnore]
         public virtual ICollection<Personal>? GetPersonals { get; set; } = new HashSet<Personal>();
-
-        
     }
 
     /// <summary>
@@ -43,8 +63,4 @@ namespace KozossegiAPI.Models
         public Friend friendship { get; set; }
     }
 
-    public class Friend_notificationId : Friend
-    {
-        public int NotificationId { get; set; }
-    }
 }
