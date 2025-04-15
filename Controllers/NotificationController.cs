35b03f84e530a057f02a1239d9c0b7b6cd912c78
pistii <pilisi.istvan@gmail.com>
2025-04-15 15:52:34 +0200

    Set up notification controller

--- Controllers/NotificationController.cs
+++ Controllers/NotificationController.cs
@@ -1,12 +1,16 @@
-﻿using KozossegiAPI.Interfaces;
+﻿using KozossegiAPI.Auth.Helpers;
+using KozossegiAPI.Interfaces;
 using KozossegiAPI.Models;
+using KozossegiAPI.Repo;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace KozossegiAPI.Controllers
 {
     [ApiController]
     [Route("/api/[controller]")]
-    public class NotificationController : ControllerBase
+    public class NotificationController : BaseController<NotificationController>
     {
         private readonly INotificationRepository _notificationRepository;
 
@@ -16,16 +20,22 @@ namespace KozossegiAPI.Controllers
             _notificationRepository = notificationRepository;
         }
 
-        [HttpGet("{userId}/{currentPage}")]
-        [HttpGet("{userId}")]
-        public async Task<IActionResult> GetAll(int userId, int currentPage = 1, int itemPerRequest = 10)
+
+
+        [Authorize]
+        [HttpGet("getAll")]
+        public async Task<IActionResult> GetAll(
+            [FromQuery(Name = "currentPage")] int currentPage = 1,
+            [FromQuery(Name = "itemPerRequest")] int itemPerRequest = 10)
         {
-            var notifications = await _notificationRepository.GetAll_PersonNotifications(userId);
+            int userId = GetUserId();
+
+            var notifications = await _notificationRepository.GetAllNotifications(userId);
 
             if (notifications == null) 
-                return null;
+                return NotFound();
 
-            var few = _notificationRepository.Paginator<NotificationWithAvatarDto>(notifications, currentPage, itemPerRequest);
+            var few = _notificationRepository.Paginator<GetNotification>(notifications, currentPage, itemPerRequest);
                         
             if (notifications.Count > 0) 
                 return Ok(few);
@@ -33,16 +43,22 @@ namespace KozossegiAPI.Controllers
             return NotFound();
         }
 
-        [HttpGet("notificationRead/{notificationId}")]
-        public async Task<IActionResult> NotificationRead(int notificationId)
+        [HttpGet("read/{notificationId}")]
+        public async Task<IActionResult> NotificationRead(string notificationId)
         {
-            //The method is called when the user clicks on a notification, and marks it as not a new one.
             try
             {
-                var result = await _notificationRepository.GetByIdAsync<Notification>(notificationId);
-                if (result.isNew)
+                var result = await _notificationRepository.GetByPublicIdAsync<Notification>(notificationId);
+                //var userNotification = await _notificationRepository.GetWithIncludeAsync<UserNotification, Notification>(u => u.notification, u => u.NotificationId == result.Id);
+                var userNotification = await _notificationRepository.GetByIdAsync<UserNotification>(result.Id);
+
+                if (result == null)
                 {
-                    result.isNew = false;
+                    return NotFound();
+                }
+                else if (result.UserNotification.FirstOrDefault(n => n.NotificationId == result.Id).IsRead)
+                {
+                    result.UserNotification.FirstOrDefault(n => n.NotificationId == result.Id).IsRead = true;
                     await _notificationRepository.UpdateThenSaveAsync(result);
                     return Ok();
                 }
--- Interfaces/INotificationRepository.cs
+++ Interfaces/INotificationRepository.cs
@@ -4,10 +4,9 @@ namespace KozossegiAPI.Interfaces
 {
     public interface INotificationRepository : IGenericRepository<Notification>
     {
-        Task RealtimeNotification(int toUserId, GetNotification dto);
         Task<List<GetNotification>> GetAllNotifications(int userId);
-        Task<Notification> CreateNotification(CreateNotification cn);
         Task UpdateNotification(CreateNotification cn);
-        Task<Notification> CreateFriendRequestAcceptedNotification(CreateNotification createNotification);
+        Task SendNotification(int receiverUserId, Personal author, CreateNotification createNotification);
+        Task<Notification> CreateNotification(Personal author, CreateNotification cn);
     }
 }
