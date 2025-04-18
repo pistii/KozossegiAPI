9325f9b9a45809d4a93b78c4e5031c9160f857b8
pistii <pilisi.istvan@gmail.com>
2025-04-18 10:48:02 +0200

    Remove unused method

--- Interfaces/IImageRepository.cs
+++ Interfaces/IImageRepository.cs
@@ -5,8 +5,6 @@ namespace KozossegiAPI.Interfaces
 {
     public interface IImageRepository
     {
-        Task<MediaContent> GetPostImage(int postId);
-        Task<List<PostDto>> GetAll(int userId, int currentPage = 1, int requestItems = 9);
         Task UpdateDatabaseImageUrl(int userId, string url);
     }
 }
--- Repo/ImageRepository.cs
+++ Repo/ImageRepository.cs
@@ -18,50 +18,6 @@ namespace KozossegiAPI.Repo
             _ctx = dbContext;
         }
 
-        public async Task<List<PostDto>> GetAll(int userId, int currentPage = 1, int requestItems = 9)
-        {
-            var postsWithImage = await _ctx.PersonalPost
-                .Include(p => p.Posts.MediaContent)
-                .Include(p => p.Posts.PostComments)
-                .Where(p => p.Posts.SourceId == userId && p.Posts.MediaContent != null) //Ennyiben tér el a sima post kérelemtől, hogy még ezt kell vizsgálni
-                .OrderByDescending(_ => _.Posts.DateOfPost)
-                .Take(requestItems)
-                .Select(p => new PostDto
-                {
-                    PersonalPostId = p.personalPostId,
-                    FullName = $"{p.Personal_posts.firstName} {p.Personal_posts.middleName} {p.Personal_posts.lastName}",
-                    PostId = p.Posts.Id,
-                    AuthorAvatar = p.Personal_posts.avatar!,
-                    AuthorId = p.Personal_posts.id,
-                    Likes = p.Posts.Likes,
-                    Dislikes = p.Posts.Dislikes,
-                    DateOfPost = p.Posts.DateOfPost,
-                    PostContent = p.Posts.PostContent!,
-                    PostComments = p.Posts.PostComments
-                        .Select(c => new CommentDto
-                        {
-                            CommentId = c.commentId,
-                            AuthorId = c.FK_AuthorId,
-                            CommenterFirstName = _ctx.Personal.First(_ => _.id == c.FK_AuthorId).firstName!,
-                            CommenterMiddleName = _ctx.Personal.First(_ => _.id == c.FK_AuthorId).middleName!,
-                            CommenterLastName = _ctx.Personal.First(_ => _.id == c.FK_AuthorId).lastName!,
-                            CommenterAvatar = _ctx.Personal.First(_ => _.id == c.FK_AuthorId).avatar!,
-                            CommentDate = c.CommentDate,
-                            CommentText = c.CommentText!
-                        })
-                        .ToList(),
-                    MediaContent = p.Posts.MediaContent
-                })
-                .ToListAsync();
-            return postsWithImage;
-
-        }
-
-        public async Task<MediaContent> GetPostImage(int postId)
-        {
-            return await _ctx.MediaContent.FirstOrDefaultAsync(c => c.MediaContentId == postId);
-        }
-
         public async Task UpdateDatabaseImageUrl(int userId, string url)
         {
             var user = await _ctx.Personal.FirstOrDefaultAsync(u => u.id == userId);
